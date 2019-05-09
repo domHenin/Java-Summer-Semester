872a99f66dd5d71c59db26b271d67c52a79d748d
domHenin <domhenin.517@gmail.com>
2019-05-09 10:20:34 -0400

    initial setup

---
+++ src/com/valencia/professor/macon/modules/h3_beer_assignment/HwThree.java
@@ -0,0 +1,66 @@
+package com.valencia.professor.macon.modules.h3_beer_assignment;
+
+public class HwThree {
+    public static void main(String[] args) {
+        System.out.println("thank you GOD for who you are in my life!");
+    }
+}
+
+//TODO:
+// Outcomes:
+// Student will demonstrate the ability to write and run a simple program.
+// Student will demonstrate the ability to perform mathematical calculations in Java.
+// Student will demonstrate the ability to IO from/to user.
+// ..
+// Program Specifications:
+// Citation
+// ..
+// All the factoids about beer and exercise given in this assignment are paraphrased from realbeer.com.
+// ..
+// Problem A:  Cost of Beer
+// Studies have shown that, on average, someone who consumes a single 12-ounce can of beer every day without
+// compensating for the calorie intake through diet modifications or extra exercise, will gain approximately 15 pounds
+// in one year. You can assume this ratio of beer-to-weight-gain is constant. (For example, doubling the beer
+// consumption to two 12-ounce cans per day will lead to 30 pounds of extra weight in one year.)
+// ..
+// You will have to ask the user how many beers he or she expects to consume each day, on average, as well as the
+// average amount of money he or she spends on a single 12-ounce can of beer. Using this information, your goal is to
+// write a program to determine the following:
+// ..
+// How many beers will the user consume over the course of the year?
+// Assuming a beer is 150 calories, how many calories will the user take in from beer alone over the course of the year?
+// How much weight can the user expect to gain in one year based on the number of 12-ounce beers they consume every day?
+// How much the user will spend on beer this year?
+// ..
+// All output should be displayed using 2 digits of precision after the decimal point!
+// ..
+// You may assume that there are 365 days in a year (ignoring leap years).
+// ..
+// Input Specification
+// The number of 12-ounce beers consumed each day will be a non-negative real number.
+// How much the user spends on a single beer, on average, will be a positive real number. (Some people like cheap beer,
+// while others pay a bit more for their favorite brand.)
+// ..
+// Output Sample
+// ..
+// Here is one sample output of running the program. Note that this test is NOT a comprehensive test. You should test
+// your program with different data than is shown here based on the specifications given. The user input is given in
+// italicswhile the program output is in bold.
+// ..
+// Sample Run #1
+// On average, how many beers will you consume each day?
+// 2.5
+// On average, how much will you pay for each can of beer?
+// 1.25
+// That is approximately 912.50 beers in one year.
+// ..
+// In one year, you will consume approximately 136875.00 calories from beer alone.
+// ..
+// Without diet or exercise to counter these calories, you can expect to gain 37.50 pounds from drinking that much beer this year.
+// ..
+// Submission Requirements:
+// Create your design tool in a separate file that you add to your Java project.
+// Compress all files from this project including the design tool.
+// Submit your compressed project.
+// Be sure to specify in your submission comments whether you did you work in Mac or Windows.
+
