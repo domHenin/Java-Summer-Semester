de59955f4fe1c40a0aea7d170e6886ccf30bece7
domHenin <domhenin.517@gmail.com>
2019-05-21 18:40:17 -0400

    wip

---
+++ src/com/valencia/professor/macon/modules/hw4_methods_IO_Exceptions/HwFour.java
@@ -0,0 +1,59 @@
+package com.valencia.professor.macon.modules.hw4_methods_IO_Exceptions;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class HwFour {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+//        System.out.println("have mercy upon me, oh GOD of abraham, issac, and jacob");
+
+//        System.out.println("Enter a number");
+
+        int firNum=2;
+        int secNum=0;
+
+
+//        secNum = firNum + firNum;
+//        secNum =+ firNum;
+
+        firNum =+ secNum;
+
+        System.out.println(firNum);
+
+
+
+//NOTES: left here:: attempting to add firNum to itself then output. goodLuck()
+
+
+
+//        System.out.println("Choose an option: ");
+//        int option = 0;
+//
+//        switch (option) {
+//            case 1:
+//                System.out.println("Enter first name: ");
+//                String userName = scanner.nextLine();
+//                break;
+//            case 2:
+//                System.out.println("Enter age: ");
+//                int userAge = scanner.nextInt();
+//                int getAge += userAge;
+//        }
+    }
+}
+
+
+//TODO:
+// your assignment is to write a menu driven program.  The menu will be responded to using a switch statement.
+// The menu options (cases) will be as follows (You should give them more succinct labels in your program):
+// ..
+// Read the user’s first name and echo it back out 20 times.
+// Read the user’s age and double it and display the age and the doubled age.
+// Read the user's age and output one of the following statements:
+// Since you are xx years old, you are a teenager
+// Since you are xx years old, you are NOT a teenager   (***of course xx displays the user's age)
+// Read a single integer between 3 and 50 from the user. Create a triangle of X’s with the as many rows as the value
+// of the user's input.  The triangle needs to be displayed on the screen and in a text document named triangle.txt.
