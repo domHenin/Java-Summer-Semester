6def43d9096ab83d0a99bc1f2af42df4ce8f1dc9
domHenin <domhenin.517@gmail.com>
2019-05-28 10:22:13 -0400

    initial setup

---
+++ src/com/valencia/professor/macon/modules/hw5_1D_array_assignment/HwFive.java
@@ -0,0 +1,33 @@
+package com.valencia.professor.macon.modules.hw5_1D_array_assignment;
+
+public class HwFive {
+    public static void main(String[] args) {
+//        System.out.println("blessed in the name of JESUS, the name above all names.");
+
+        int[] myArray = new int[100];
+
+
+    }
+}
+
+//TODO:
+//        Program Specifications:
+//        Write a program that does the following:
+// ..
+//        Create an array of 100 integers.
+//        Store 100 random integers (between 1 and 100) in the array.
+//        Print out the elements of the array.
+//        Sort the array in ascending order.
+//        Print out the sorted array.
+//        Prompt the user to enter a number between 1 and 100. Search the array for that number and then display  "Found" or "Not Found" message.
+//        Display each number from 1 to 100 and the number of times each is found in the array.
+//        Display the average of the numbers in the array.
+//        Display the highest number in the array.
+//        Display the lowest number in the array.
+// ..
+//        Other requirements.
+// ..
+//        Everything should be done using METHODS.
+//        NO NEED FOR A MENU SYSTEM. Just call the methods in the sequence above.
+//        Add your design tool to your project.
+
