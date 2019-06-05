4eee7515a43d8ba5f7ddacce8af0502bb557b59b
domHenin <domhenin.517@gmail.com>
2019-06-05 10:01:16 -0400

    update

--- src/com/valencia/professor/macon/modules/hw5_1D_array_assignment/HwFive.java
+++ src/com/valencia/professor/macon/modules/hw5_1D_array_assignment/HwFive.java
@@ -12,10 +12,14 @@ package com.valencia.professor.macon.modules.hw5_1D_array_assignment;
 
 import java.util.Random;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class HwFive {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-//        System.out.println("blessed in the name of JESUS, the name above all names.");
+//       System.out.println("blessed is the name of JESUS, the name above all names.");
 
         double[] myArray = new double[10];
         int max=100;
@@ -25,13 +29,15 @@ public class HwFive {
         double minArray = Integer.MAX_VALUE;
 
 
+
+
 //  Randomize Array.
         for (int i=1; i< myArray.length; i++) {
             myArray[i] = (Math.random() * range) + min;
             System.out.printf("%.0f\n", myArray[i]);
         }
 
-// WORKING ON SORT ASCENDING: https://www.tutorialspoint.com/java/util/arrays_sort_double.htm
+// WORKING ON SORT ASCENDING: https://www.tutorialspoint.com/java/util/arrays_sort_double.htm --> COMPLETE
         Arrays.sort(myArray);
         System.out.println("Sorted Array: ");
 
@@ -42,8 +48,7 @@ public class HwFive {
 
 
 // Max & Min Array.
-        for(int i = 1; i < myArray.length; i++)
-        {
+        for(int i = 1; i < myArray.length; i++) {
             if(myArray[i] > maxArray) {
                 maxArray = myArray[i];
             }
@@ -54,6 +59,7 @@ public class HwFive {
         }
         System.out.printf("\nMax Array Value: %.0f\n", maxArray);
         System.out.printf("Min Array Value: %.0f\n", minArray);
+
 // WORKING ON PRINTING OUT MINIMUM ARRAY.
 
 //        for (int i=1; i < myArray.length; i++) {
@@ -67,7 +73,28 @@ public class HwFive {
         result = calculateAverage(myArray);
 
         System.out.printf("The average is: %.0f\n", result);
-    }
+
+//test: userInput
+        System.out.println("enter a number: [1-100]");
+
+        double choice = scanner.nextDouble();
+
+        if (choice >= 1 && choice <= 100) {
+            System.out.println("searching for number");
+        } else {
+            System.out.println("ERROR!");
+        }
+
+
+
+        for (choice : myArray) {
+
+
+            System.out.println(choice);
+        }
+
+
+    } //end of MAIN.
 
 // METHOD: calculate the average.
     static double calculateAverage(double[] array) {
@@ -82,8 +109,23 @@ public class HwFive {
         return average;
     }
 
-////METHOD: sort the array.
-//    static
+// METHOD: sort the array.
+
+//METHOD: userChoice
+    static double obtainInput(int choice) {
+        choice = scanner.nextInt();
+
+        System.out.println("Enter a number [1-100]");
+
+        if (choice >=1 || choice <= 100) {
+            System.out.println("inside 'if statement in method'");
+        } else {
+            System.out.println("ERROR!");
+        }
+
+        return choice;
+    }
+
 }
 
 //TODO:
@@ -93,13 +135,13 @@ public class HwFive {
 //        Create an array of 100 integers.
 //        Store 100 random integers (between 1 and 100) in the array.
 //        Print out the elements of the array.
-//        Sort the array in ascending order.
-//        Print out the sorted array.
+//        Sort the array in ascending order. --> COMPLETE
+//        Print out the sorted array. --> COMPLETE
 //        Prompt the user to enter a number between 1 and 100. Search the array for that number and then display  "Found" or "Not Found" message.
 //        Display each number from 1 to 100 and the number of times each is found in the array.
-//        Display the average of the numbers in the array.
-//        Display the highest number in the array.
-//        Display the lowest number in the array.
+//        Display the average of the numbers in the array. --> COMPLETE
+//        Display the highest number in the array. --> COMPLETE
+//        Display the lowest number in the array. --> COMPLETE
 // ..
 //        Other requirements.
 // ..
