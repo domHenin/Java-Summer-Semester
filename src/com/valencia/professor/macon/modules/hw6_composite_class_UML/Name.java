18c9492750e73e7bfa67e44649dd1d3210fa359c
domHenin <domhenin.517@gmail.com>
2019-06-18 10:24:52 -0400

    hw6-wip

---
+++ src/com/valencia/professor/macon/modules/hw6_composite_class_UML/Name.java
@@ -0,0 +1,27 @@
+package com.valencia.professor.macon.modules.hw6_composite_class_UML;
+
+public class Name {
+    private String firName, middName, lasName;
+
+//  SETTERS.
+    public void setFirName(String firName) { this.firName = firName; }
+    public void setMiddName(String middName) { this.middName = middName; }
+    public void setLasName(String lasName) { this.lasName = lasName; }
+
+//  GETTERS.
+    public String getFirName() { return firName; }
+    public String getMiddName() { return middName; }
+    public String getLasName() { return lasName; }
+
+    
+}
+
+//TODO
+// A class that defines a Name (first name, middle name, and last name).
+//        Then, define all setters and getters, constructors and toString methods.
+// Rules:
+// Gender must have three possible values: M, F or O (other).
+// Age must be between 1 and 120.
+// Salary must be a positive number.
+// The first and last name must be in sentence case. (Billy Armstrong)
+// A middle name is optional (the code will account for this)
