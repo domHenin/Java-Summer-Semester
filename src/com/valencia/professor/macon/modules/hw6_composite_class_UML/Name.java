c2863afa215fab7d181fa7b47f9cf6e049ee4661
domHenin <domhenin.517@gmail.com>
2019-06-21 14:03:24 -0400

    total reWork, complete TODO's

--- src/com/valencia/professor/macon/modules/hw6_composite_class_UML/Name.java
+++ src/com/valencia/professor/macon/modules/hw6_composite_class_UML/Name.java
@@ -1,4 +1,4 @@
-package com.valencia.professor.macon.modules.hw6_composite_class_UML;
+package src.com.valencia.professor.macon.modules.hw6_composite_class_UML;
 
 import java.util.Scanner;
 
@@ -9,37 +9,46 @@ public class Name {
     private String name;
 
     private String firName;
+    private String middName;
     private String lasName;
 
     //  SETTERS.
     public void setName(String name) { this.name=name; }
     public void setFirName(String firName) { this.firName = firName; }
+
+    public void setMiddName(String middName) { this.middName = middName; }
+
     public void setLasName(String lasName) { this.lasName = lasName; }
 
     //  GETTERS.
     public String getName() { return name; }
     public String getFirName() { return firName; }
+
+    public String getMiddName() { return middName; }
+
     public String getLasName() { return lasName; }
 
 
-    public void grabName() {
-//        String choice;
-
-        System.out.println("Enter Full Name: ");
-        name = scanner.nextLine();
-
-        firName=name.substring(0, name.indexOf(" "));
-        lasName=name.substring(name.indexOf(" ")+1);
-
-        String cap = Character.toString(firName.charAt(0)).toUpperCase();
-        firName = cap + firName.substring(1);
-
-        cap = Character.toString(lasName.charAt(0)).toUpperCase();
-        lasName=cap + lasName.substring(1);
-
-        System.out.println("Name: "+firName+" "+lasName);
-        System.out.println();
-    }
+//TODO: clean up code. delete if not using logic
+//TODO: maybe use logic for the 'n' option to capitalize as requested.
+//    public void grabName() {
+////        String choice;
+//
+//        System.out.println("Enter Full Name: ");
+//        name = scanner.nextLine();
+//
+//        firName=name.substring(0, name.indexOf(" "));
+//        lasName=name.substring(name.indexOf(" ")+1);
+//
+//        String cap = Character.toString(firName.charAt(0)).toUpperCase();
+//        firName = cap + firName.substring(1);
+//
+//        cap = Character.toString(lasName.charAt(0)).toUpperCase();
+//        lasName=cap + lasName.substring(1);
+//
+//        System.out.println("Name: "+firName+" "+lasName);
+//        System.out.println();
+//    }
 }
 
 //TODO
@@ -53,7 +62,7 @@ public class Name {
 // A middle name is optional (the code will account for this)
 
 
-
+//TODO: delete if no use
 //CODE GRAVEYARD::
 //  middle name LOGIC ATTEMPT
 //    public String grabName() {
