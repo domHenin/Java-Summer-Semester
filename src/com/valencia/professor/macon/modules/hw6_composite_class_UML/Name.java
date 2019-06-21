435912d7be8510494eae31a5f8fb92da2a919449
domHenin <domhenin.517@gmail.com>
2019-06-21 19:20:56 -0400

    completed

--- src/com/valencia/professor/macon/modules/hw6_composite_class_UML/Name.java
+++ src/com/valencia/professor/macon/modules/hw6_composite_class_UML/Name.java
@@ -1,54 +1,21 @@
-package src.com.valencia.professor.macon.modules.hw6_composite_class_UML;
+package com.valencia.professor.macon.modules.hw6_composite_class_UML;
 
-import java.util.Scanner;
 
 public class Name {
-
-    Scanner scanner = new Scanner(System.in);
-
-    private String name;
 
     private String firName;
     private String middName;
     private String lasName;
 
     //  SETTERS.
-    public void setName(String name) { this.name=name; }
     public void setFirName(String firName) { this.firName = firName; }
-
     public void setMiddName(String middName) { this.middName = middName; }
-
     public void setLasName(String lasName) { this.lasName = lasName; }
 
     //  GETTERS.
-    public String getName() { return name; }
     public String getFirName() { return firName; }
-
     public String getMiddName() { return middName; }
-
     public String getLasName() { return lasName; }
-
-
-//TODO: clean up code. delete if not using logic
-//TODO: maybe use logic for the 'n' option to capitalize as requested.
-//    public void grabName() {
-////        String choice;
-//
-//        System.out.println("Enter Full Name: ");
-//        name = scanner.nextLine();
-//
-//        firName=name.substring(0, name.indexOf(" "));
-//        lasName=name.substring(name.indexOf(" ")+1);
-//
-//        String cap = Character.toString(firName.charAt(0)).toUpperCase();
-//        firName = cap + firName.substring(1);
-//
-//        cap = Character.toString(lasName.charAt(0)).toUpperCase();
-//        lasName=cap + lasName.substring(1);
-//
-//        System.out.println("Name: "+firName+" "+lasName);
-//        System.out.println();
-//    }
 }
 
 //TODO
@@ -60,43 +27,3 @@ public class Name {
 // Salary must be a positive number.
 // The first and last name must be in sentence case. (Billy Armstrong)
 // A middle name is optional (the code will account for this)
-
-
-//TODO: delete if no use
-//CODE GRAVEYARD::
-//  middle name LOGIC ATTEMPT
-//    public String grabName() {
-//        String choice;
-//
-//        System.out.println("Do you have a Middle Name? [Y]es / [N]o");
-//        choice=scanner.next();
-//
-//        if (choice.equals("y")) {
-//            System.out.println("Enter First Name: ");
-////            setFirName(scanner.nextLine());
-//            firName=scanner.nextLine();
-//
-//            scanner.nextLine();
-//
-//            System.out.println("Enter Middle Name: ");
-////            setMiddName(scanner.nextLine());
-//            middName=scanner.nextLine();
-//
-//            System.out.println("Enter Last Name: ");
-////            setLasName(scanner.nextLine());
-//        } else if (choice.equals("n")) {
-//            System.out.println("Enter First Name: ");
-//            firName=scanner.nextLine();
-//
-//            scanner.nextLine();
-//
-//            System.out.println("Enter Last Name: ");
-//        }
-//
-//        return getFirName();
-//    }
-
-//    public String grabName(String firName){
-//        setFirName(scanner.nextLine());
-//    }
-//}
--- src/com/valencia/professor/macon/modules/hw6_composite_class_UML/Person.java
+++ src/com/valencia/professor/macon/modules/hw6_composite_class_UML/Person.java
@@ -1,11 +1,7 @@
-package src.com.valencia.professor.macon.modules.hw6_composite_class_UML;
+package com.valencia.professor.macon.modules.hw6_composite_class_UML;
 
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Person {
-
-    Scanner scanner = new Scanner(System.in);
 
  //  VARIABLES:
     private String gender;
@@ -22,65 +18,13 @@ public class Person {
     public String getGender() { return gender; }
     public int getAge() { return age; }
     public double getSalary() { return salary; }
-
-
-//TODO: clean up code. delete if not using logic
-    public void grabSalary() {
-
-        System.out.println("Enter Salary: ");
-        salary = scanner.nextDouble();
-        scanner.next();
-//        scanner.nextLine();
-//      boolean salaryScope = false;
-
-//        if (salary>=0) {
-//            System.out.println("Salary: "+salary);
-//        } else {
-//            System.out.println("ERROR! "+salary+" out of scope.");
-//        }
-
-//        return salary;
-    }
-
-//    public String grabGender() {
-////        String result;
-//
-//        System.out.println("Enter Gender(M[ale], F[emale], O[ther]): ");
-//        gender = scanner.nextLine();
-////        setGender(scanner.nextLine());
-//
-//        if (gender=="M" || gender=="m") {
-////            return "M[ale]";
-//            System.out.println("MALE");
-//        } else if (gender=="F" || gender=="f") {
-////            return "F[emale]";
-//            System.out.println("FEMALE");
-//        }
-//        return gender;
-//    }
-
-
-//    public void grabAge() {
-//        System.out.println("Enter Age:");
-//        age = scanner.nextInt();
-//
-//        do {
-//            if (age<=120) {
-//                return;
-//            } else {
-//                System.out.println("ERROR! "+age+" out of scope");
-//                System.out.println("Enter age: ");
-//                age=scanner.nextInt();
-//            }
-//        } while (age>121);
-//        scanner.nextLine();
-//    }
 } //end of Person
 
 //TODO:
-//        A class that defines a Person (name, gender, age, and salary).     (You can add other classes if you want.)
-//        Then, define all setters and getters, constructors and toString methods.
-//        Then, create and attach a UML for the Person and Name classes.
+// A class that defines a Person (name, gender, age, and salary).     (You can add other classes if you want.)
+// Then, define all setters and getters, constructors and toString methods.
+// Then, create and attach a UML for the Person and Name classes.
+// ..
 // Rules:
 // Gender must have three possible values: M, F or O (other).
 // Age must be between 1 and 120.
