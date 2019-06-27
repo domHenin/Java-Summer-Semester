688a516b1558b8dc67dc21c3e30bce293a8f942a
domHenin <domhenin.517@gmail.com>
2019-06-26 22:34:17 -0400

    TODO: complete logic for cat's lives

---
+++ src/com/valencia/professor/macon/modules/hw7_inheritance_oop/Bird.java
@@ -0,0 +1,39 @@
+package com.valencia.professor.macon.modules.hw7_inheritance_oop;
+
+public class Bird extends Animal {
+    private double wingspan;
+    private boolean fly;
+
+    public Bird(double height, double weight, double wingspan, boolean fly) {
+        super(weight, height);
+        setWingspan(wingspan);
+        this.wingspan=wingspan;
+        this.fly=fly;
+    }
+
+//SETTERS:
+    public void setWingspan(double wingspan) { this.wingspan=wingspan; }
+    public void setFly(boolean fly) { this.fly=fly; }
+
+//GETTERS:
+    public double getWingspan() { return wingspan; }
+    public boolean getFly() { return fly; }
+
+//toString:
+    public String toString() {
+        return "Bird: "+super.toString();
+    }
+
+
+}
+
+
+// TODO:
+// Program Specifications:
+// Then, add the following:
+// Animals have a Weight.
+// Animals have a Height.
+// Bird is an Animal
+// Birds have a wing span
+// Birds have a canFly which is true or false (some birds cannot fly)
+// Create a test class that creates one of each type of animal and displays the animal’s toString method.
---
+++ src/com/valencia/professor/macon/modules/hw7_inheritance_oop/HwSeven.java
@@ -0,0 +1,45 @@
+package com.valencia.professor.macon.modules.hw7_inheritance_oop;
+
+public class HwSeven {
+    public static void main(String[] args) {
+//        System.out.println("to GOD all glory!");
+
+        Animal animal = new Animal(120, 5);
+
+        System.out.println(animal);
+
+        Dog dog = new Dog(90, 3, "gizmo", "german sheperd", 25);
+
+        System.out.println(dog);
+
+        Bird bird = new Bird(35, 20, 90, true);
+
+        System.out.println(bird);
+
+        Cat cat = new Cat(12, 15, "shadow", 9);
+
+        System.out.println(cat);
+
+    }
+}
+
+
+// TODO:
+// Program Specifications:
+// Start by watching Video Segment 16 from Dr. Colin Archibald's video series (found in the module overview(.  Key in the program shown in the video and make sure it works.
+//        Then, add the following:
+//
+//
+//        Animals have a Weight.
+//        Animals have a Height.
+//        Dog is an Animal.
+//        Dogs have a Name.
+//        Dogs have a Breed.
+//        Dogs have a DOB.
+//        Cat is an Animal
+//        Cats have a Name.
+//        Cats have 9 lives, so you need to keep track of the remaining lives once a cat dies.
+//        Bird is an Animal
+//        Birds have a wing span
+//        Birds have a canFly which is true or false (some birds cannot fly)
+//        Create a test class that creates one of each type of animal and displays the animal’s toString method.
